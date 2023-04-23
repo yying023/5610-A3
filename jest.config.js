523f693ffe50d17ba4d52fa16c06b9dac29b91da
yying023 <yying023@outlook.com>
2023-04-22 21:30:53 -0700

    add files3

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    // 匹配的文件路径，该路径匹配器配置可在这里进行设置
-    testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
-  
-    // 要转换的文件类型，这里使用 Babel 转换器将 ES6+ 转换为 ES5
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-      },
-  
-    // 覆盖默认的测试运行器，这里我们将测试结果转换为 Jest 默认的格式
-    testResultsProcessor: "jest-sonar-reporter",
-  };
+  setupFilesAfterEnv: ["<rootDir>/src/test/setupTests.js"],
+  transform: {
+    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+  testMatch: ["<rootDir>/src/test/**/*.test.(js|jsx|ts|tsx)"],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy"
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  verbose: true
+};
